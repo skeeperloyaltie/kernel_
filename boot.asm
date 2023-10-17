d45b271c24a42268688772fa1524c3d3a0959842
skeeperloyaltie <gugod254@gmail.com>
2023-10-17 08:22:53 +0300

    jesus christ - asm is very weird

--- boot.asm
+++ boot.asm
@@ -1,8 +1,39 @@
 ORG 0x7c00
 BITS 16
-; This is the bootloader. It loads a kernel into memory and jumps to it, passing along any arguments given on the command line (if there
 start:
+    mov si, message
+    call print
+
+
+    jmp $
+print:
+    mov bx, 0
+.loop:
+
+    lodsb ;load the si register
+    cmp al, 0
+    je .done
+
+    call print_char
+    jmp .loop
+
+.done:
+    ret
+
+print_char:
     mov ah, 0eh
-    mov al, 'A'
     int 0x10
-    
+    ret
+  
+    int 0x10
+
+message: db 'Hello world!', 0
+
+times 510- ($ - $$) db 0 
+dw 0xAA55
+
+
+
+
+
+
